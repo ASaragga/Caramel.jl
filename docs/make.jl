e979dfd1b1bd3ba0c060f3a7dfea12eb7874e6a6
Antonio Saragga Seabra <47437960+ASaragga@users.noreply.github.com>
2025-03-11 22:51:21 +0000

    Add files via upload

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,23 @@
+using Caramelo
+using Documenter
 
+DocMeta.setdocmeta!(Caramelo, :DocTestSetup, :(using Caramelo); recursive=true)
+
+makedocs(;
+    modules=[Caramelo],
+    authors="Antonio Saragga Seabra",
+    sitename="Caramelo.jl",
+    format=Documenter.HTML(;
+        canonical="https://ASaragga.github.io/Caramelo.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/ASaragga/Caramelo.jl",
+    devbranch="main",
+)
